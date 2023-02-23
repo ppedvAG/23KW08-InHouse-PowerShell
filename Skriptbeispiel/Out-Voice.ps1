b69cfa4c6f30f3204120f35844fabc4989cbfde5
Stefan Ober <StefanO@ppedv.de>
2023-02-23 14:51:15 +0100

    .Net

---
+++ Skriptbeispiel/Out-Voice.ps1
@@ -0,0 +1,25 @@
+﻿[cmdletBinding()]
+param(
+[ValidateLength(5,100)]
+[string]$Eingabe = "Hallo"
+)
+
+$audiosvc = Get-Service -Name Audiosrv
+do
+{
+if($audiosvc.Status -ne "Running")
+{
+    $audiosvc.Start()
+}
+Start-Sleep -Milliseconds 50
+$audiosvc.Refresh()
+}until($audiosvc.Status -eq "Running")
+
+
+Add-Type -AssemblyName System.Speech
+
+$speaker = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+$speaker.SelectVoice("Microsoft Zira Desktop")
+$speaker.Speak($Eingabe)
+
+
