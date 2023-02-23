79110a08fdd29bc425299648b78d3905d738c2f2
Stefan Ober <StefanO@ppedv.de>
2023-02-23 08:57:38 +0100

    BeginProcessEnd

---
+++ Skriptbeispiel/BeginProcessEnd.ps1
@@ -0,0 +1,18 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true, ValueFromPipeLineByPropertyName=$true,ValueFromPipeline=$true)]
+[string[]]$Name
+)
+
+Begin
+{
+    Write-Host -ForegroundColor Magenta -Object "Start - Einmalig"
+}
+PRocess
+{
+    Write-Host -ForegroundColor (Get-Random -Minimum 0 -Maximum 15) -Object $Name
+}
+End
+{
+    Write-Host -ForegroundColor Magenta -Object "Ende - Einmalig"
+}
