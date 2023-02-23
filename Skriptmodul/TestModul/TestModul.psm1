4e65e6a34467e4ab42a5fdc3dce12046d84d33e1
Stefan Ober <StefanO@ppedv.de>
2023-02-23 11:43:17 +0100

    ModulÜbung

---
+++ Skriptmodul/TestModul/TestModul.psm1
@@ -0,0 +1,30 @@
+﻿function Export-RandomAdUser
+{
+[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[ValidateScript({Test-Path -Path $PSItem -Pathtype Container})]
+[string]$Path
+)
+if($Path.EndsWith("\") -ne $true)
+{
+    $Path += "\"
+}
+
+$User = Get-ADUser -Filter {Enabled -eq $true} | Get-Random 
+
+[string]$FileName
+if($User.GivenName.length -lt 1 -and $User.SurName.Length -lt 1)
+{
+    $FileName = $User.Name
+}
+else
+{
+    $FileName = $User.GivenName + $User.SurName
+}
+
+$FileName += ".csv"
+
+Export-Csv -InputObject $User -Path ($Path + $FileName)
+
+}
