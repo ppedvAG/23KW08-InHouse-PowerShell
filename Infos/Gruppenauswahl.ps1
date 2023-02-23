3df07ead0e9883c0874756924b8255d5b32df391
Stefan Ober <StefanO@ppedv.de>
2023-02-23 11:44:15 +0100

    Code Snippet Gruppenauswahl

---
+++ Infos/Gruppenauswahl.ps1
@@ -0,0 +1,19 @@
+﻿#Dieses CideSnippet gibt alle User aus die in Gruppe 1 sind aber nicht in Gruppe 2 Mitglied sind
+
+$group = Get-ADGroupMember -Identity "SG"
+$SGA = Get-ADGroup -Identity "SGA"
+
+foreach($member in $group)
+{
+    $User = Get-ADUser -Identity $member -Properties MemberOf
+    if($User.MemberOf -contains $SGA.DistinguishedName)
+    {
+        Write-Host -Object "ISt in beiden" -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host -Object $User.Name -ForegroundColor Red
+    }    
+}
+
+
