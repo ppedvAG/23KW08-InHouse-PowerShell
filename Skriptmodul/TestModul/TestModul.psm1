b9aeac325a535ef365789132d1494701ee810562
Stefan Ober <StefanO@ppedv.de>
2023-02-23 14:51:51 +0100

    Write-Debug

--- Skriptmodul/TestModul/TestModul.psm1
+++ Skriptmodul/TestModul/TestModul.psm1
@@ -28,3 +28,24 @@ $FileName += ".csv"
 Export-Csv -InputObject $User -Path ($Path + $FileName)
 
 }
+
+function Test-Debug
+{
+[cmdletBinding()]
+param()
+
+$Ausgabe1 = "ERste Ausgabe"
+Write-Debug -Message "Vor erster Ausgabe"
+Write-Host -ForegroundColor Red -Object $Ausgabe1
+
+
+$Ausgabe2 = "Zweite Ausgabe"
+Write-Debug -Message "Vor zweiter Ausgabe"
+Write-Host -ForegroundColor Green -Object $Ausgabe2
+
+
+$Ausgabe3 = "Dritte Ausgabe"
+Write-Debug -Message "Vor dritter Ausgabe"
+Write-Host -ForegroundColor Cyan -Object $Ausgabe3
+
+}
