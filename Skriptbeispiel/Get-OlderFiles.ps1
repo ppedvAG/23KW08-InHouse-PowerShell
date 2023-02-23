4b8079c59b6fdf2d3313eeb6b55262b7dc7bdabe
Stefan Ober <StefanO@ppedv.de>
2023-02-23 15:41:43 +0100

    BeispielSkript Pfad aufräumen

---
+++ Skriptbeispiel/Get-OlderFiles.ps1
@@ -0,0 +1,20 @@
+﻿[cmdletBinding()]
+param(
+[ValidateScript({Test-Path -Path $PSItem -PathType Container})]
+[string]$Path,
+
+[ValidateRange(30,90)]
+[int]$Days = 30
+)
+
+$Items = Get-ChildItem -Path $Path -Recurse 
+
+foreach($item in $Items)
+{
+    $EarliestKeep =  (Get-Date).AddDays(($Days * -1))
+
+    if($Item.LastAccessTime -lt $EarliestKeep)
+    {
+        Write-Host -Object $Item.FullName
+    }
+}
